bcb52661f24ac691b779b101026c8fe3343cae91
piotrsieradzki <piotr.sieradzki03@gmail.com>
2024-11-20 15:26:31 +0100

    Homework after Lab

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+class CarWithLombok {
+    private String model;
+    private String brand;
+    private String color;
+    private int year;
+    private int price;
+    private int stock;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        CarWithLombok car1 = new CarWithLombok("GLE", "Mercedes", "white", 2020, 350000, 5);
+        CarWithLombok car2 = new CarWithLombok("GLC", "Mercedes", "black", 2023, 200000, 3);
+
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car1.getColor());
+        System.out.println("Equals: " + car1.equals(car2));
+        System.out.println("HashCode car1: " + car1.hashCode());
+        System.out.println("HashCode car2: " + car2.hashCode());
+    }
+}
